4776c69b75a0811d1ac1b184a85779b52c484a7b
Tri Minh Cao <trimcao@gmail.com>
2016-05-18 10:14:01 +0700

    added comments and docstrings.

--- placeTree.py
+++ placeTree.py
@@ -13,15 +13,11 @@ class Gate:
     """
     this class represents a gate in the circuits
     parameters include: delay, arrival time, etc.
+    Interconnections between gates will be handled by Grid class.
     """
     def __init__(self, name, delay = 0, IO = False):
         self._name = name
-        # in the program, we will initialize all gates first, then we add
-        # information about connections
-
         # initialize inputs and outputs
-        self._inputs = set() # use set to prevent duplication
-        self._outputs = set()
         self._delay = delay
         self._isIO = IO
 
@@ -47,9 +43,8 @@ class TopoSort:
     def sort(self, gates, outputs):
         """
         Sort the gates topologically based on outputs
-        Input: gates is the list of gates; outputs is a dict containing inter-
+        input: gates is the list of gates; outputs is a dict containing inter-
         connections data of these gates
-        Note: probably we need a class to do topoSort properly
         Need to add temporary marked
         """
         self.unvisited = set(gates)
@@ -117,36 +112,29 @@ class Grid:
         We represent the grid by a 2d list, each position in the grid, in turn,
         is a list of gates (one position may have multiple gates).
 
+        height and width: dimensions of the grid
+        gates: a list of gates in the grid
         ios: list of IOs gates and locations
+            format: a tuple (IOgate, location tuple)
         inCons and outCons: dicts of interconnections of the gates, these dicts
             can be provided during initialization.
         """
         self._height = height
         self._width = width
         self._grid = [[set() for i in range(height)] for j in range(width)]
-        #self._inputs = inputs
-        #self._outputs = outputs
         self._IOLocs = dict()
-        # NOTE: probably don't need to differentiate inputs from outputs, they
-        # work the same
         # process inputs and outputs
         for each in ios:
             self._IOLocs[each[0]] = each[1]
-        # NOTE: also need to store the locations of all gates
+        # store the locations of all gates in the locs dict
         self._locs = dict()
         self._gates = gates
+        # add IOs to the self._gates list
         self._gates.extend(self._IOLocs.keys())
-        # Note: current idea is to build delay tables for all gates, including
-        # IOs
-
         self._inCons = inCons
         self._outCons = outCons
-
         # we need to sort each time we do a new placement
         self._topoSorter = TopoSort()
-        # NOTE: must re-write the following line. Must initialize the interconnections
-        # within Grid class.
-        #self._gates = topoSorter.sort(self._gates, outputs)
 
     def __str__(self):
         s = ''
@@ -164,6 +152,9 @@ class Grid:
         return s
 
     def fill(self, gate, col, row):
+        """
+        This method is used to add a gate to a location in the resulting grid.
+        """
         # only use this method at the end of placement process
         self._grid[col][row].add(gate)
         self._locs[gate] = (col, row)
@@ -181,45 +172,69 @@ class Grid:
         return self._IOLocs[IO]
 
     def getLoc(self, gate):
+        """
+        Return the location of a gate
+        """
         return self._locs[gate]
 
     def addIn(self, gate, inp):
+        """
+        Add a fanin of a gate
+        """
         if gate in self._inCons:
             self._inCons[gate].add(inp)
         else:
             self._inCons[gate] = set([inp])
 
     def addOut(self, gate, out):
+        """
+        Add a fanout of a gate
+        """
         if gate in self._outCons:
             self._outCons[gate].add(out)
         else:
             self._outCons[gate] = set([out])
 
     def numIn(self, gate):
+        """
+        Return number of fanins of a gate
+        """
         if gate in self._inCons:
             return len(self._inCons[gate])
         else:
             return 0
 
     def numOut(self, gate):
+        """
+        Return number of fanouts of a gate
+        """
         if gate in self._outCons:
             return len(self._outCons[gate])
         else:
             return 0
 
     def getOutputs(self, gate):
+        """
+        Return the fanouts of a gate as a list.
+        """
         if gate in self._outCons:
             return self._outCons[gate]
         else:
             return set()
 
     def getInputs(self, gate):
+        """
+        Return the fanins of a gate as a list.
+        """
         if gate in self._inCons:
             return self._inCons[gate]
         else:
             return set()
 
     def topoSort(self):
+        """
+        Call the topological sort method.
+        """
         self._gates = self._topoSorter.sort(self._gates, self._outCons)
 
 
@@ -238,7 +253,6 @@ gatesExam = [v1, v2, v3]
 #inputs = [(in1, (0, 0)), (in2, (0, 3))]
 #outputs = [(out, (4, 1))]
 ios = [(in1, (0, 0)), (in2, (0, 3)), (out, (4, 1))]
-
 
 # test Grid class
 gridTest = Grid(4, 5, ios, gatesExam)
@@ -274,34 +288,45 @@ class Placement:
     It will return an grid with gates in optimal locations.
     """
     def __init__(self, grid, delayModel):
+        """
+        grid: the grid needed to do placement
+        delayModel: the function used to compute the delay between two gates
+        """
         self._grid = grid
         self._delayModel = delayModel
+        # represent the delay tables by dictionaries
         self._delayTables = dict()
-        # probably delay table should be a dict, not a 2d array
         self._locations = []
         for i in range(grid.getWidth()):
             for j in range(grid.getHeight()):
                 self._locations.append((i, j))
-        #table = [[float('inf') for i in range(grid.getHeight())] for j in range(grid.getWidth())]
-        # call topoSort
+        # call topoSort to sort the gates topologically
         self._grid.topoSort()
         self._gates = grid.getGates()
+        # initialize the locOpt, a dict to store the optimal location for each gate
         self._locOpt = dict()
-        #self._possibleLocs = dict()
         for each in self._gates:
             self._delayTables[each] = dict()
-            # initialize the locOpt
             self._locOpt[each] = dict()
             for loc in self._locations:
                 self._locOpt[each][loc] = dict()
 
     def getLocations(self):
+        """
+        Return all possible locations from the grid as a list
+        """
         return self._locations
 
     def setLocOpt(self, outGate, locOut, inGate, locIn):
+        """
+        Set the optimal location for the fanin while doing placement
+        """
         self._locOpt[outGate][locOut][inGate] = locIn
 
     def setDelay(self, gate, loc, delay):
+        """
+        Set the optimal delay for a location in the delay table
+        """
         self._delayTables[gate][loc] = delay
 
     def buildTables(self):
@@ -310,14 +335,15 @@ class Placement:
         """
         # call topoSort
         self._grid.topoSort()
+        # need to update the self._gates because the sorting method only sort
+        # the gate list in the Grid object.
         self._gates = self._grid.getGates()
         height = self._grid.getHeight()
         width = self._grid.getWidth()
         locations = self.getLocations()
         possibleLocs = []
         for each in self._gates:
-            # Need to find a way to store locations of fanins
-            # idea: each gate will have another dict. Format of information:
+            # each gate will have another dict. Format of information:
             # locOpt[currentGate][loc][fanin] = optimal loc for that fanin
             # inside locOpt[currentGate] is another dict
             if each.isIO():
@@ -329,11 +355,9 @@ class Placement:
                 # initialize to find the maxDelay
                 maxDelay = each.getDelay()
                 for fanin in self._grid.getInputs(each):
-                #for fanin in each.getInputs():
                     minDelay = float('inf')
                     minInLoc = (-1, -1)
-                    # probably we only need to consider the locations present
-                    # in the delay table
+                    # we only need to consider the locations present in the delay table
                     for inLoc in self._delayTables[fanin].keys():
                         inDelay = self._delayTables[fanin][inLoc]
                         delay = inDelay + self._delayModel(loc, inLoc) + each.getDelay()
@@ -352,6 +376,9 @@ class Placement:
                 self.setDelay(each, loc, maxDelay)
 
     def getDelayTable(self, gate):
+        """
+        Get the delay table in list format
+        """
         table = [[float('inf') for i in range(self._grid.getHeight())] for j in range(self._grid.getWidth())]
         for loc in self._delayTables[gate].keys():
             table[loc[0]][loc[1]] = self._delayTables[gate][loc]
@@ -361,9 +388,12 @@ class Placement:
         return self._locOpt[outGate][locOut][inGate]
 
     def place(self):
-        # idea: for IOs, just put them to their fixed positions
+        """
+        This method will do the actual placement using the delay tables.
+        """
         for i in range(len(self._gates) - 1, -1, -1):
             currentGate = self._gates[i]
+            # put the IOs to their fixed location
             if (currentGate.isIO()):
                 loc = self._grid.getIOLoc(currentGate)
                 self._grid.fill(currentGate, loc[0], loc[1])
