f1f2fe3bb25a67e238436f7ab9766187d8c00baf
Tri Minh Cao <trimcao@gmail.com>
2016-06-03 10:46:30 +0700

    add delayLookup table for different cells

---
+++ delayLookup.py
@@ -0,0 +1,15 @@
+"""
+Delay Lookup Table for different cells 
+Name: Tri Minh Cao
+Email: tricao@utdallas.edu
+Date: June 2016
+"""
+
+table = {}
+table['NAND'] = 30
+table['NOR'] = 40
+table['AND'] = 57
+table['NOT'] = 29
+table['XOR'] = 109
+table['BUFF'] = 48
+table['OR'] = 75
